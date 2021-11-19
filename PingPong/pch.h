e611291bcf65ad7a76ede979d8b03059a0463907
sacyz1n <sacyz1n@naver.com>
2021-11-20 02:51:46 +0900

    [11-20] pch 추가

---
+++ PingPong/pch.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+#include <map>
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <process.h>
+#include <exception>
+#include <iostream>
+#include <tchar.h>
+#include <deque>
+#include <queue>
+#include <fstream>
+#include <unordered_map>
+#include <cassert>
+#include <functional>
+#include <array>
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "mswsock.lib")
